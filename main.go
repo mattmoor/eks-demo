ce8979592e05388e76ada5e7c2432f593d2637ab
Matt Moore <mattmoor@chainguard.dev>
2022-01-21 13:45:45 -0800

    edit app

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("helloworld: received a request")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "Demo"
+		target = "Chainguard"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", target)
 }
